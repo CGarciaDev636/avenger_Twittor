e3e40642461bf246a62449b54a3870f44545f93c
CGarciaDev636 <loquendark@gmail.com>
2024-11-02 13:02:33 -0600

    actualizacion de practica

--- sw.js
+++ sw.js
@@ -32,3 +32,33 @@ self.addEventListener('install', e =>{
     
     e.waitUntil(Promise.all([cacheStatic,cacheInmutable]))
 })
+
+
+self.addEventListener('activate', e =>{
+    const respuesta= caches.keys().then(keys =>{
+        keys.forEach(key =>{
+         if(key !== STATIC_CACHE && key.includes('static')){
+                return caches.delete(key)
+            }
+         if(key !== DYNAMIC_CACHE && key.includes('dynamic')){
+            return caches.delete(key)
+            }
+        })
+    })
+    e.waitUntil(respuesta)
+})
+
+self.addEventListener('fetch', e =>{
+    const respuesta=caches.match(e.request).then(res =>{
+        if (res){
+            return res
+        }
+        else{
+            return fetch(e.request).then(newRes =>{
+                return actualizarCacheDinamico(DYNAMIC_CACHE, e.request,newRes)
+            })
+        }
+
+    })
+    e.respondWith(respuesta)
+})
